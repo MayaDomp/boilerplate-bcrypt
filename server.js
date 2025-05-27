5ca275472665b16fc7aaa20e171018e96cded922
MayaDomp <Dompierremaya83@gmail.com>
2025-05-27 18:21:49 -0400

    change

--- server.js
+++ server.js
@@ -7,11 +7,15 @@ fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
-
+const bcrypt = require('bcrypt');
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  /*Store hash in your db*/
+});
+bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+  /*res == true or false*/
+});
 
 //END_ASYNC
 
